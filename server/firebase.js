e6def42165d479472ab9f2c64d51313e9ea9d9ff
codemeron1 <wondelrosario@gmail.com>
2025-11-03 15:32:21 +0800

    refactor: Update firebase.js to improve configuration handling and ensure fbKey.json is created if missing

--- server/firebase.js
+++ server/firebase.js
@@ -1,10 +1,15 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import dotenv from "dotenv";
+dotenv.config();
 
 import fs from 'fs';
-
+if (!fs.existsSync("./fbKey.json")) {
+  const jsonString = Buffer.from(process.env.CONFIG_JSON_BASE64, "base64").toString();
+  fs.writeFileSync("./fbKey.json", jsonString);
+}
 const serviceAccount = JSON.parse(
-    fs.readFileSync("./fbKey.json", "utf8")
+  fs.readFileSync("./fbKey.json", "utf8")
 );
 
 const app = initializeApp({
